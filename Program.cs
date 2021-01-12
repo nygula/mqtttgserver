20ccc92bca8dae73f0215825314be48fb331b244
nygula <569878931@qq.com>
2021-01-12 17:06:23 +0800

    235

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Agent;
+using NewLife.MQTT;
+using NewLife;
+using NewLife.Log;
+using NewLife.Serialization;
+using NewLife.Threading;
+using NewLife.MQTT.Messaging;
+using NewLife.Security;
+
+namespace Castle.Mqtt.TGServer
+{
+    class Program
+    {
+        static void Main(string[] args) =>new MyService().Main(args);
+
+    }
+
+  
+
+    public class MyService : ServiceBase
+    {
+        public MqttClient _MqttClient;
+        public MyService()
+        {
+         
+            XTrace.UseConsole();
+            ServiceName = ServerConfig.Current.ServerName;
+
+
+
+        }
+
+        TimerX timer1;
+      
+
+        // TimerX timer1;
+        public void InitMqttClient()
+        {
+            var cli = new MqttClient()
+            {
+                ClientId = Guid.NewGuid() + "",
+                Server = ServerConfig.Current.MqttServer, //  mqtt://192.168.1.222:1883
+                KeepAlive = 6,                             //  保证6秒内是在线的.
+                UserName = ServerConfig.Current.MqttUserName, // admin
+                Password = ServerConfig.Current.MqttPassword,  // 123456
+                Log = XTrace.Log,
+                Timeout = 6000
+            };
+            XTrace.WriteLine($"[配置ClientToJson]:{cli.ToJson()}");
+            cli.ConnectAsync();
+            cli.SubscribeAsync(ServerConfig.Current.PullTopTic, (e) =>
+            {
+                XTrace.WriteLine("[订阅]:" + ServerConfig.Current.PullTopTic + ".[消息]:" + e.Payload.ToStr());
+            });
+            _MqttClient = cli;
+        }
+
+     
+        public void CheckMqttClient()
+        {
+            _MqttClient.Server.Host = ServerConfig.Current.MqttServer;
+            XTrace.WriteLine("[检查]:"+_MqttClient.ToJson());
+        }
+       
+        protected override void StartWork(string reason)
+        {
+            
+            
+             InitMqttClient();
+            
+          
+            timer1 = new TimerX(a => CheckMqttClient(), null, 2000, 2000) { Async = true };
+            base.StartWork(reason);
+        }
+        protected override void StopWork(string reason)
+        {
+            if (!_MqttClient.Disposed)
+            {
+                _MqttClient.DisconnectAsync();
+                _MqttClient.Dispose();
+            }
+           timer1.Dispose();
+
+            base.StopWork(reason);
+        }
+    }
+}
---
+++ ServerConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Configuration;
+namespace Castle.Mqtt.TGServer
+{
+    public class ServerConfig:Config<ServerConfig>
+    {
+        [DisplayName("系统服务名称")]
+        public string ServerName { set; get; } = "CastleMqttTGServer";
+
+        [DisplayName("MQTT服务器地址")]
+        public string MqttServer { set; get; } = "mqtt://192.168.1.70:1883";
+
+
+        [DisplayName("MQTT帐号")]
+
+        public string MqttUserName { set; get; } = "admin";
+        [DisplayName("MQTT密码")]
+
+        public string MqttPassword { set; get; } = "123456";
+
+
+        [DisplayName("MQTT订阅主题")]
+
+        public string PullTopTic { set; get; } = "/xindoo/tg/v2/#";
+            
+    }
+}
